a04eabf5a50919a43704d102604222ef9dba077b
Michael Yoo <michael@yoo.id.au>
2015-02-09 22:52:27 +1030

    Launching on Pagoda Box!

---
+++ web/api.php
@@ -0,0 +1,501 @@
+<?php
+require_once(__DIR__ . "/../vendor/autoload.php");
+
+//Fix for https://github.com/codeguy/Slim/pull/993 until new version is released
+$obj = new Slim\Http\Response();
+$refObject = new ReflectionObject($obj);
+$refProperty = $refObject->getProperty("messages");
+$refProperty->setAccessible(true);
+$array = $refProperty->getValue(null);
+$array[429] = '429 Too Many Requests';
+$refProperty->setValue(null, $array);
+unset($obj, $refObject, $refProperty, $array);
+
+use Slim\Middleware;
+
+/** @property Redis $redis */
+class Slim extends Slim\Slim {}
+
+class Error
+{
+	// Validation Errors
+	const AUTH_TOKEN_NOT_PROVIDED = 10;
+	const AUTH_TOKEN_TOO_LONG = 11;
+	const COMMAND_TOO_LONG = 12;
+	const SERVER_DETAIL_PARSE_FAILED = 13;
+	const IP_VALIDATION_FAILED = 14;
+	const PORT_VALIDATION_FAILED = 15;
+	const COMMAND_NOT_PROVIDED = 16;
+
+	// Remote Host Errors
+	const TIMEOUT_CONNECT = 20;
+	const INVALID_PACKET = 21;
+	const BAD_PASSWORD = 22;
+	const BANNED = 23;
+	const BAD_RESPONSE = 24;
+
+	// Rate Limiting Errors
+	const TOO_MANY_REQUESTS = 30;
+	const TOO_MANY_REQUESTS_SERVER = 31;
+}
+
+class HTTPTokenAuthMiddleware extends Middleware
+{
+	public function call()
+	{
+		$request = $this->app->request();
+
+		if($request->headers->get("Authorization") === NULL or substr($request->headers->get("Authorization"), 0, 5) !== "token")
+		{
+			$this->next->call();
+			return; // Not HTTP Token Authentication - Bypass
+		}
+
+		$token = trim(substr($request->headers->get("Authorization"), 6));
+
+		$this->app->container->set("token", $token);
+
+		$this->next->call();
+	}
+}
+
+class HTTPMethodHEADStripBodyMiddleware extends Middleware
+{
+	public function call()
+	{
+		$request = $this->app->request();
+
+		$this->next->call();
+
+		if($request->getMethod() === "HEAD")
+		{
+			$this->app->response->setBody("");
+		}
+	}
+}
+
+/*
+ * Two types of rate limiting: one for the connecting client, and one for the server host.
+ *
+ * This is so that an attacker cannot use us as a reflector using multiple IPs.
+ */
+class RateLimit
+{
+	public static function attemptServer($ip)
+	{
+		/** @var Slim $app */
+		$app = Slim::getInstance();
+
+		$shortIP = ip2long($ip);
+
+		if($shortIP === false)
+		{
+			// It should have been validated before-hand.
+			throw new LogicException("Server IP is not a valid IP address for rate limiting.");
+		}
+
+		if(($count = $app->redis->get("ratelimit:server:{$shortIP}")) !== false)
+		{
+			if($count >= 6) // 6 requests in 3 seconds
+			{
+				$app->halt(429, json_encode([
+					"error" => [
+						"code" => Error::TOO_MANY_REQUESTS_SERVER,
+						"message" => "You have exceeded your maximum allowed requests per second to the specified host."
+					]
+				])); // HTTP/1.1 429 Too Many Requests (RFC 6585)
+			}
+			else
+			{
+				$app->redis->incr("ratelimit:server:{$shortIP}");
+			}
+		}
+		else
+		{
+			//Key didn't exist
+			$app->redis->multi(); // Atomic Operation
+			$app->redis->incr("ratelimit:server:{$shortIP}");
+			$app->redis->expire("ratelimit:server:{$shortIP}", 3);
+			$app->redis->exec(); // We could use a SET with a timeout instead bu meh, until it becomes a bottleneck...
+		}
+	}
+}
+
+function requireTokenAuth ()
+{
+	$app = Slim::getInstance();
+
+	if(!$app->container->has("token"))
+	{
+		$app->halt(401, json_encode([
+			"error" => [
+				"code" => Error::AUTH_TOKEN_NOT_PROVIDED,
+				"message" => "Authorization Token is required for this route, but was not provided."
+			]
+		]));
+	}
+}
+
+function rateLimit()
+{
+	/** @var Slim $app */
+	$app = Slim::getInstance();
+
+	$shortIP = ip2long($app->request()->getIp());
+
+	if($shortIP === false)
+	{
+		throw new RuntimeException("Remote IP is not a valid IP address for rate limiting.");
+	}
+
+	if(($count = $app->redis->get("ratelimit:client:{$shortIP}")) !== false)
+	{
+		if($count >= 2) // How many requests in a given timeframe
+		{
+			$app->halt(429, json_encode([
+				"error" => [
+					"code" => Error::TOO_MANY_REQUESTS,
+					"message" => "You have exceeded your maximum allowed requests per second."
+				]
+			])); // HTTP/1.1 429 Too Many Requests (RFC 6585)
+		}
+		else
+		{
+			$app->redis->incr("ratelimit:client:{$shortIP}");
+		}
+	}
+	else
+	{
+		//Key didn't exist
+		$app->redis->multi(); // Atomic Operation
+		$app->redis->incr("ratelimit:client:{$shortIP}");
+		$app->redis->expire("ratelimit:client:{$shortIP}", 3); // Timeframe
+		$app->redis->exec(); // We could use a SET with a timeout instead bu meh, until it becomes a bottleneck...
+	}
+}
+
+$app = new Slim(array(
+	"mode" => "production",
+	"debug" => false
+));
+
+if(!isset($_ENV["SLIM_APP_MODE"]) or $_ENV["SLIM_APP_MODE"] !== "production")
+{
+		if(isset($_ENV["SLIM_APP_MODE"]))
+	{
+		$app->config("mode", strtolower($_ENV["SLIM_APP_MODE"]));
+	}
+	else
+	{
+		$app->config("mode", "development");
+	}
+}
+
+if($app->config("mode") === "development")
+{
+	$_ENV["CACHE1_HOST"] = "192.168.137.5";
+	$_ENV["CACHE1_PORT"] = 6379;
+}
+
+$app->add(new HTTPTokenAuthMiddleware);
+$app->add(new HTTPMethodHEADStripBodyMiddleware);
+
+/** @property Redis $redis */
+$app->redis = new Redis;
+$app->redis->connect($_ENV["CACHE1_HOST"], $_ENV["CACHE1_PORT"], 2);
+
+$app->group("/:serverHex", function () use ($app) {
+	$app->post('/rcon', "requireTokenAuth", "rateLimit", function ($serverHex) use ($app) {
+		$sourceQuery = new SourceQuery;
+
+		$token = $app->container->get("token");
+
+		$command = hex2bin(trim($app->request()->getBody()));
+
+		// If RCON password is longer than 4096 bytes, it's probably a buffer overflow attack using
+		// our service as a reflector.
+		if(mb_strlen($token) > 4096)
+		{
+			$app->halt(400, json_encode([
+				"error" => [
+					"code" => Error::AUTH_TOKEN_TOO_LONG,
+					"message" => "The provided Authorization Token was too long."
+				]
+			]));
+		}
+
+		// Similar to above
+		if(mb_strlen($command) > 4096)
+		{
+			$app->halt(400, json_encode([
+				"error" => [
+					"code" => Error::COMMAND_TOO_LONG,
+					"message" => "The provided request body (the command) was too long."
+				]
+			]));
+		}
+
+		if($command === "")
+		{
+			$app->halt(400, json_encode([
+				"error" => [
+					"code" => Error::COMMAND_NOT_PROVIDED,
+					"message" => "The request body (the command) was not provided."
+				]
+			]));
+		}
+
+		//From: 3132372e302e302e313a3139313332
+		$serverAddressAndPort = hex2bin($serverHex);
+		//To: 127.0.0.1:19132
+
+		$serverAddress = explode(":", $serverAddressAndPort);
+
+		// Validate User Input
+		if(count($serverAddress) !== 2 or !isset($serverAddress[0]) or !isset($serverAddress[1]))
+		{
+			$app->halt(400, json_encode([
+				"error" => [
+					"code" => Error::SERVER_DETAIL_PARSE_FAILED,
+					"message" => "There was an issue with parsing the provided server details."
+				]
+			]));
+		}
+
+		if(filter_var($serverAddress[0], FILTER_VALIDATE_IP) === false)
+		{
+			$app->halt(400, json_encode([
+				"error" => [
+					"code" => Error::IP_VALIDATION_FAILED,
+					"message" => "The validation of the provided IP address failed."
+				]
+			]));
+		}
+
+		if(ctype_digit($serverAddress[1]) === false or ((integer) $serverAddress[1] < 0) or ((integer) $serverAddress[1] > 65535))
+		{
+			$app->halt(400, json_encode([
+				"error" => [
+					"code" => Error::PORT_VALIDATION_FAILED,
+					"message" => "The validation of the provided IP address failed."
+				]
+			]));
+		}
+
+		$server = new stdClass();
+		$server->address = $serverAddress[0];
+		$server->port = $serverAddress[1];
+
+		RateLimit::attemptServer($server->address);//TODO: Move failure logic out
+
+		try
+		{
+			$sourceQuery->Connect($server->address, $server->port, 3, SourceQuery::SOURCE);
+			$sourceQuery->SetRconPassword($token);
+
+			$result = trim($sourceQuery->Rcon($command));
+
+			$sourceQuery->Disconnect();
+		}
+		//catch(\xPaw\SourceQuery\Exception\InvalidArgumentException $e) // Don't catch this so it errors out
+		//catch(\xPaw\SourceQuery\Exception\SocketException $e) // Don't catch this so it errors out
+		catch(\xPaw\SourceQuery\Exception\TimeoutException $e)
+		{
+			switch($e->getCode())
+			{
+				case \xPaw\SourceQuery\Exception\TimeoutException::TIMEOUT_CONNECT:
+					$app->halt(504, json_encode([
+						"error" => [
+							"code" => Error::TIMEOUT_CONNECT,
+							"message" => "The attempt to connect to the provided server timed out."
+						]
+					])); // HTTP/1.1 504 Gateway Timeout
+					break;
+				default:
+					throw new LogicException("Unknown TiemoutException Exception Code");
+			}
+		}
+		catch(\xPaw\SourceQuery\Exception\InvalidPacketException $e)//Bad packet from game server
+		{
+			$app->halt(502, json_encode([
+				"error" => [
+					"code" => Error::INVALID_PACKET,
+					"message" => "The provided server responded with an invalid packet."
+				]
+			])); // HTTP/1.1 502 Bad Gateway
+		}
+		catch(\xPaw\SourceQuery\Exception\AuthenticationException $e)
+		{
+			switch($e->getCode())
+			{
+				case \xPaw\SourceQuery\Exception\AuthenticationException::BAD_PASSWORD:
+					$app->halt(401, json_encode([
+						"error" => [
+							"code" => Error::BAD_PASSWORD,
+							"message" => "The authentication against the provided server with the given credentials failed."
+						]
+					])); // HTTP/1.1 401 Unauthorized
+					break;
+				case \xPaw\SourceQuery\Exception\AuthenticationException::BANNED:
+					$app->halt(403, json_encode([
+						"error" => [
+							"code" => Error::BANNED,
+							"message" => "The provided server actively refused connection against the specific client. ".
+								"This could be due to the an IP ban against the WebRCON.io servers."
+						]
+					])); // HTTP/1.1 403 Forbidden
+				break;
+				default:
+					throw new LogicException("Unknown AuthenticationException Exception Code");
+			}
+		}
+
+		if(isset($result) and $result !== false)
+		{
+			// Note here that $result could be an empty string
+
+			$result = str_replace("\r\n", "\n", $result);
+			$output = explode("\n", $result);
+
+			echo json_encode([
+				"response" => $output// Each new line is on a new array key
+			]);
+		}
+		else
+		{
+			$app->halt(502, json_encode([
+				"error" => [
+					"code" => Error::BAD_RESPONSE,
+					"message" => "The provided server responded with an invalid response (false) to the provided command."
+				]
+			])); // HTTP/1.1 502 Bad Gateway :D
+		}
+	});
+
+	$app->post('/authenticate', "requireTokenAuth", "rateLimit", function ($serverHex) use ($app) {
+		$sourceQuery = new SourceQuery;
+
+		$token = $app->container->get("token");
+
+		// If RCON password is longer than 4096 bytes, it's probably a buffer overflow attack using
+		// our service as a reflector.
+		if(mb_strlen($token) > 4096)
+		{
+			$app->halt(400, json_encode([
+				"error" => [
+					"code" => Error::AUTH_TOKEN_TOO_LONG,
+					"message" => "The provided Authorization Token was too long."
+				]
+			]));
+		}
+
+		//From: 3132372e302e302e313a3139313332
+		$serverAddressAndPort = hex2bin($serverHex);
+		//To: 127.0.0.1:19132
+
+		$serverAddress = explode(":", $serverAddressAndPort);
+
+		// Validate User Input
+		if(count($serverAddress) !== 2 or !isset($serverAddress[0]) or !isset($serverAddress[1]))
+		{
+			$app->halt(400, json_encode([
+				"error" => [
+					"code" => Error::SERVER_DETAIL_PARSE_FAILED,
+					"message" => "There was an issue with parsing the provided server details."
+				]
+			]));
+		}
+
+		if(filter_var($serverAddress[0], FILTER_VALIDATE_IP) === false)
+		{
+			$app->halt(400, json_encode([
+				"error" => [
+					"code" => Error::IP_VALIDATION_FAILED,
+					"message" => "The validation of the provided IP address failed."
+				]
+			]));
+		}
+
+		if(ctype_digit($serverAddress[1]) === false or ((integer) $serverAddress[1] < 0) or ((integer) $serverAddress[1] > 65535))
+		{
+			$app->halt(400, json_encode([
+				"error" => [
+					"code" => Error::PORT_VALIDATION_FAILED,
+					"message" => "The validation of the provided IP address failed."
+				]
+			]));
+		}
+
+		$server = new stdClass();
+		$server->address = $serverAddress[0];
+		$server->port = $serverAddress[1];
+
+		RateLimit::attemptServer($server->address);//TODO: Move failure logic out
+
+		try
+		{
+			$sourceQuery->Connect($server->address, $server->port, 3, SourceQuery::SOURCE);
+			$sourceQuery->SetRconPassword($token);
+
+			$sourceQuery->Disconnect();
+		}
+		//catch(\xPaw\SourceQuery\Exception\InvalidArgumentException $e) // Don't catch this so it errors out
+		//catch(\xPaw\SourceQuery\Exception\SocketException $e) // Don't catch this so it errors out
+		catch(\xPaw\SourceQuery\Exception\TimeoutException $e)
+		{
+			switch($e->getCode())
+			{
+				case \xPaw\SourceQuery\Exception\TimeoutException::TIMEOUT_CONNECT:
+					$app->halt(504, json_encode([
+						"error" => [
+							"code" => Error::TIMEOUT_CONNECT,
+							"message" => "The attempt to connect to the provided server timed out."
+						]
+					])); // HTTP/1.1 504 Gateway Timeout
+					break;
+				default:
+					throw new LogicException("Unknown TiemoutException Exception Code");
+			}
+		}
+		catch(\xPaw\SourceQuery\Exception\InvalidPacketException $e)//Bad packet from game server
+		{
+			$app->halt(502, json_encode([
+				"error" => [
+					"code" => Error::INVALID_PACKET,
+					"message" => "The provided server responded with an invalid packet."
+				]
+			])); // HTTP/1.1 502 Bad Gateway
+		}
+		catch(\xPaw\SourceQuery\Exception\AuthenticationException $e)
+		{
+			switch($e->getCode())
+			{
+				case \xPaw\SourceQuery\Exception\AuthenticationException::BAD_PASSWORD:
+					$app->halt(401, json_encode([
+						"error" => [
+							"code" => Error::BAD_PASSWORD,
+							"message" => "The authentication against the provided server with the given credentials failed."
+						]
+					])); // HTTP/1.1 401 Unauthorized
+					break;
+				case \xPaw\SourceQuery\Exception\AuthenticationException::BANNED:
+					$app->halt(403, json_encode([
+						"error" => [
+							"code" => Error::BANNED,
+							"message" => "The provided server actively refused connection against the specific client. ".
+								"This could be due to the an IP ban against the WebRCON.io servers."
+						]
+					])); // HTTP/1.1 403 Forbidden
+					break;
+				default:
+					throw new LogicException("Unknown AuthenticationException Exception Code");
+			}
+		}
+
+		echo json_encode([
+			"authenticated" => true
+		]);
+	});
+});
+
+$app->run();
---
+++ web/console.js
@@ -0,0 +1,400 @@
+var TEXTCOLOUR_RED = "\x1b[31;1m";
+var TEXTCOLOUR_GREEN = "\x1b[32;1m";
+var TEXTCOLOUR_RESET = "\x1b[39;49m";
+
+$(document).ready(function () {
+	var initialInfo = [
+		"--------------------------------------------------",
+		"| WebRCON.io Version v1.0.0                      |",
+		"| Developed and Maintained by @sekjun9878        |",
+		"| For any bugs please contact michael@yoo.id.au  |",
+		"--------------------------------------------------",
+
+		"",
+
+		"--------------------------------------------------",
+		"| Now you will need to enter your server's       |",
+		"| details. Please note that all connection to    |",
+		"| our website is secured using HTTPS and that    |",
+		"| we do not keep logs of any communication.      |",
+		"--------------------------------------------------",
+
+		""
+	];
+
+	/*
+	 * 0 for white cursor
+	 * 1 for green cursor
+	 */
+	var alternating = 0;
+
+	var term = new Terminal({
+		cols: 145,
+		rows: 40,
+		screenKeys: true
+	});
+
+	var commandInputLine = $('#term-input');
+	var currentTimeIndicator = $('#current-time');
+	var termInputPrefix = $("#term-input-prefix-info");
+	var termTitle = $("#term-title");
+
+	/*
+		0 for uninitialised
+		1 for awaiting server address
+		2 for awaiting server port
+		3 for awaiting server password
+		4 for authenticating
+		5 for connected
+	 */
+	var status = 0;
+
+	var address;
+	var port;
+
+	var token = "";//RCON password
+
+	term.on("data", function(data) {
+		var preLineText = commandInputLine.text();//The text of the input field when this function was called,
+		//regardless of its current state
+
+		if (data == "\x7f") {
+			commandInputLine.text(preLineText.substr(0, preLineText.length - 1));
+
+			if(status == 3) // Password Input
+			{
+				token = token.slice(0, - 1);
+			}
+		} else if (data == "\r") {
+			commandInputLine.text("");
+
+			if (preLineText == "//reload")
+			{
+				term.destroy();
+				commandInputLine.text("Reloading...");
+				location.reload();
+			}
+
+			switch(status)
+			{
+				case 0: // Uninitialised
+					break;
+				case 1: // Prompt for address
+					if(!validateIPAddress(preLineText))
+					{
+						term.log(TEXTCOLOUR_RED + "You have entered an invalid IP address!" + TEXTCOLOUR_RESET);
+						term.log(TEXTCOLOUR_RED + "Validation Error: Regex test failed." + TEXTCOLOUR_RESET);
+						term.log("");
+						promptServerDetails(0);
+						break;
+					}
+
+					address = preLineText;
+					termInputPrefix.text(" > ");
+					term.log("Server IP address has been set to: "+ address);
+					term.log("");
+					status = 2;
+					promptServerDetails(1);
+					break;
+				case 2: // Prompt for port
+					// Check if variable is integer
+					// http://stackoverflow.com/questions/14636536/how-to-check-if-a-variable-is-an-integer-in-javascript#comment20448200_14636652
+					if(!isNormalInteger(preLineText))
+					{
+						term.log(TEXTCOLOUR_RED + "You have entered an invalid port number!" + TEXTCOLOUR_RESET);
+						term.log(TEXTCOLOUR_RED + "Validation Error: Port number is not a real integer, or is too large." + TEXTCOLOUR_RESET);
+						term.log("");
+						promptServerDetails(1);
+						break;
+					}
+
+					if(parseInt(preLineText, 10) < 0 || parseInt(preLineText, 10) > 65535)
+					{
+						term.log(TEXTCOLOUR_RED + "You have entered an invalid port number!" + TEXTCOLOUR_RESET);
+						term.log(TEXTCOLOUR_RED + "Validation Error: Outside of range." + TEXTCOLOUR_RESET);
+						term.log("");
+						promptServerDetails(1);
+						break;
+					}
+
+					port = preLineText;
+					termInputPrefix.text(" > ");
+					term.log("Server port has been set to: "+ port);
+					term.log("");
+					status = 3;
+					promptServerDetails(2);
+					break;
+				case 3: // Prompt for password
+					//token = preLineText; token is already set below - this is done to mask the password
+					termInputPrefix.text(" > ");
+					term.log("Server password has been set to: ********");
+					term.log("");
+
+					termTitle.text("WebRcon.io by @sekjun9878, Version v1.0.0 | Authenticating " + address + ":" + port);
+					commandInputLine.text("Authenticating...");
+					term.log("Authenticating...");
+
+					status = 4;
+
+					$.ajax({
+						//TODO:                   < here is a "/" required?
+						url: window.location.href + "api.php/" + toHex(address + ":" + port) + "/authenticate",
+						type: 'POST',
+						beforeSend: function (xhr) {
+							// Set header
+							xhr.setRequestHeader("Authorization", "token " + token);
+						},
+						data: toHex("fakedata"),//TODO: Why?
+						dataType: "json",
+						success: function (body) {
+							if(typeof body == 'object' && body.hasOwnProperty("authenticated") && body.authenticated == true)
+							{
+								status = 5;
+								term.log(TEXTCOLOUR_GREEN + "Connected!" + TEXTCOLOUR_RESET);
+								//termInputPrefix.css("color", "#8ae234");//Cursor colour done in timer tick interval
+								termTitle.text("WebRcon.io by @sekjun9878, Version v1.0.0 | Connected " + address + ":" + port);
+								commandInputLine.text("");
+							}
+							else
+							{
+								// Malformed JSON request
+								term.log(TEXTCOLOUR_RED + "Authentication Failed!" + TEXTCOLOUR_RESET);
+								term.log(TEXTCOLOUR_RED + "Reason: Malformed JSON response received from server." + TEXTCOLOUR_RESET);
+								term.log(TEXTCOLOUR_RED + "This may be due to a problem with the website." + TEXTCOLOUR_RESET);
+								term.log(TEXTCOLOUR_RED + "You may either attempt to connect again, or if the problem persists," + TEXTCOLOUR_RESET);
+								term.log(TEXTCOLOUR_RED + "Please contact michael@yoo.id.au for assistance." + TEXTCOLOUR_RESET);
+								term.log("");
+
+								//Reset everything to before password input
+								termTitle.text("WebRcon.io by @sekjun9878, Version v1.0.0 | Not Connected");
+								termInputPrefix.text(" > ");
+								commandInputLine.text("");
+								token = "";
+								status = 1;
+								promptServerDetails(0);
+							}
+						},
+						error: function (xhr, ajaxOptions, thrownError) {
+							switch(xhr.status)
+							{
+								case 400: // Bad Request
+									term.log(TEXTCOLOUR_RED + "Authentication Failed!" + TEXTCOLOUR_RESET);
+									term.log(TEXTCOLOUR_RED + "Reason: Server Side Validation Failed." + TEXTCOLOUR_RESET);
+									term.log(TEXTCOLOUR_RED + "Reason: " + JSON.parse(xhr.responseText).error.message + TEXTCOLOUR_RESET);
+									term.log("");
+
+									//Reset everything to before password input
+									termTitle.text("WebRcon.io by @sekjun9878, Version v1.0.0 | Not Connected");
+									termInputPrefix.text(" > ");
+									commandInputLine.text("");
+									token = "";
+									status = 1;
+									promptServerDetails(0);
+									break;
+								case 504: // Upstream Time Out
+									term.log(TEXTCOLOUR_RED + "Authentication Failed!" + TEXTCOLOUR_RESET);
+									term.log(TEXTCOLOUR_RED + "Reason: Connection timed out." + TEXTCOLOUR_RESET);
+									term.log("");
+
+									//Reset everything to before password input
+									termTitle.text("WebRcon.io by @sekjun9878, Version v1.0.0 | Not Connected");
+									termInputPrefix.text(" > ");
+									commandInputLine.text("");
+									token = "";
+									status = 1;
+									promptServerDetails(0);
+									break;
+								case 401: // Unautorized
+									term.log(TEXTCOLOUR_RED + "Authentication Failed!" + TEXTCOLOUR_RESET);
+									term.log(TEXTCOLOUR_RED + "Reason: Incorrect Credentials." + TEXTCOLOUR_RESET);
+									term.log("");
+
+									//Reset everything to before password input
+									termTitle.text("WebRcon.io by @sekjun9878, Version v1.0.0 | Not Connected");
+									termInputPrefix.text(" > ");
+									commandInputLine.text("");
+									token = "";
+									status = 3;
+									promptServerDetails(2);
+									break;
+								case 429: // Rate Limit
+									term.log(TEXTCOLOUR_RED + "Authentication Failed!" + TEXTCOLOUR_RESET);
+									term.log(TEXTCOLOUR_RED + "Reason: Rate Limit Exceeded." + TEXTCOLOUR_RESET);
+									term.log(TEXTCOLOUR_RED + "Please wait a few seconds before trying again." + TEXTCOLOUR_RESET);
+									term.log("");
+
+									//Reset everything to before password input
+									termTitle.text("WebRcon.io by @sekjun9878, Version v1.0.0 | Not Connected");
+									termInputPrefix.text(" > ");
+									commandInputLine.text("");
+									token = "";
+									status = 3;
+									promptServerDetails(2);
+									break;
+								default:
+							}
+						}
+					});
+					break;
+				case 4:
+					commandInputLine.text("Authenticating...");//On enter, Reset text to
+					break;//Ignore input
+				case 5: // Active Connection
+					term.log(preLineText/* + data*/);//Exclude Newline (data would be newline)
+
+					//Send RCON Command
+
+					$.ajax({
+						//TODO:                   < here is a "/" required?
+						url: window.location.href + "api.php/" + toHex(address + ":" + port) + "/rcon",
+						type: 'POST',
+						beforeSend: function (xhr) {
+							// Set header
+							xhr.setRequestHeader("Authorization", "token " + token);
+						},
+						data: toHex(preLineText),
+						dataType: "json",
+						success: function (body) {
+							var response = body.response;
+							$.each(response, function (key, value) {
+								term.log(value);
+							});
+						}
+					});
+					break;
+				default:
+			}
+		} else {
+			if(status == 3) // Password Input
+			{
+				token += data;
+				data = "*";
+			}
+
+			if(status != 4) // Don't allow input when status is Authenticating
+			{
+				commandInputLine.text(preLineText + data);
+			}
+		}
+	});
+
+
+	term.on("title", function(title) {
+		termTitle.text(title);
+	});
+
+	var parent_element = document.getElementById("container-terminal");
+
+	term.open(parent_element);
+
+	term.log = function (message) {
+		var date = new Date();
+
+		term.write("\x1b[36;1m" +
+		(date.getHours()<10?'0':'') + date.getHours() + //Always display two digits, prepending 0 on single digits
+		":" + (date.getMinutes()<10?'0':'') + date.getMinutes() +
+		":" + (date.getSeconds()<10?'0':'') + date.getSeconds() +
+		"\x1b[39;49m" +
+		" " + message + "\r\n");
+	};
+
+	function initialiseTerm()
+	{
+		status = 1;//Should've been done in setInterval but just in case
+		termTitle.text("WebRcon.io by @sekjun9878, Version v1.0.0 | Not Connected");
+
+		term.write("\r\n");
+
+		commandInputLine.text("");
+
+		var x = setInterval(function () {
+			term.log(initialInfo.shift());
+
+			if(initialInfo.length == 0)
+			{
+				clearInterval(x);
+				promptServerDetails(0);
+			}
+		}, 5);//TODO: NOTE THIS VALUE
+	}
+
+	function promptServerDetails(message)
+	{
+		switch(message)
+		{
+			case 0:
+				term.log("Please enter the server IP address:");
+				termInputPrefix.text(" Server IP Address > ");
+				break;
+			case 1:
+				term.log("Please enter the server port:");
+				termInputPrefix.text(" Server Port > ");
+				break;
+			case 2:
+				term.log("Please enter the server RCON password:");
+				termInputPrefix.text(" Server RCON Password > ");
+				break;
+			default:
+		}
+	}
+
+	var prevSec = false;
+
+	setInterval(function () {
+		var date = new Date();
+
+		var seconds = date.getSeconds();
+
+		if(prevSec == false)
+		{
+			prevSec = seconds;
+		}
+
+		currentTimeIndicator.text((date.getHours()<10?'0':'') + date.getHours() + //Always display two digits, prepending 0 on single digits
+			":" + (date.getMinutes()<10?'0':'') + date.getMinutes() +
+			":" + (seconds<10?'0':'') + seconds);
+
+		if(status == 0)
+		{
+			status = 1;
+			initialiseTerm();
+		}
+
+		if(status == 5 && prevSec !== seconds) // If connected alternate cursor colour
+		{
+			if(alternating == 0) // White
+			{
+				alternating = 1;
+				termInputPrefix.css("color", "#8ae234");//Change cursor colour to green
+			}
+			else
+			{
+				alternating = 0;
+				termInputPrefix.css("color", "#f0f0f0");//Change cursor colour to white
+			}
+
+			prevSec = seconds;
+		}
+	}, 200);
+});
+
+function toHex(str) {
+	var hex = '';
+	for(var i=0;i<str.length;i++) {
+		hex += ''+str.charCodeAt(i).toString(16);
+	}
+	return hex;
+}
+
+function validateIPAddress(ipaddress)
+{
+	// http://www.w3resource.com/javascript/form/ip-address-validation.php
+	return !!/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress);
+}
+
+// Is string positive integer including 0
+// http://stackoverflow.com/a/10834843
+function isNormalInteger(str) {
+	var n = ~~Number(str);
+	return String(n) === str && n >= 0;
+}
